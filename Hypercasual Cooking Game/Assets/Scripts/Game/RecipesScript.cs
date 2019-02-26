1e3e1245a317913481b92f62fef186822d895a9e
CappyGameDev <stirrupprazak.c@gmail.com>
2019-02-26 11:14:18 +1100

    Updating file hierarchy
    Preparing recipe system recipes

--- Hypercasual Cooking Game/Assets/Scripts/Game/RecipesScript.cs
+++ Hypercasual Cooking Game/Assets/Scripts/Game/RecipesScript.cs
@@ -3,24 +3,67 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class RecipesScript : MonoBehaviour {
+    
+    //Recipe control System
+    [Header("Recipe Creation List")]
+    public Item[] recipeList;
+    [System.Serializable]
+    public class Item
+    {
+        //Required items to create the specific ingredient
+        public GameObject[] ingredients;
+        public string ingredientName;
+    }
 
     //Script References
     SpawnerScript bouncerSpawn;
 
-    //GameObject References
+    //Private GameObject References
     private GameObject recipeUI;
+
+    //Private Bool References
+    private bool recipeComplete;
 
     // Start is called before the first frame update
     public void Start()
     {
         recipeUI = GameObject.Find("RecipeUI");
         bouncerSpawn = GameObject.FindGameObjectWithTag("GameController").GetComponent<SpawnerScript>();
+        recipeComplete = false;
 
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        
+        if (recipeComplete)
+        {
+            RecipeComplete();
+        }
+    }
+
+    //Updates the Score System based on the recipe which is completed
+    public void RecipeComplete()
+    {
+
+
+
+        DecideNextRecipe();
+    }
+
+    //Loads in the next Recipe in the game
+    void LoadRecipe()
+    {
+
+    }
+
+    //Decides which Recipe will be loaded into the game
+    void DecideNextRecipe()
+    {
+        int RandInt;
+        RandInt = Random.Range(0, recipeList.Length);
+
+
+        LoadRecipe();
     }
 }
